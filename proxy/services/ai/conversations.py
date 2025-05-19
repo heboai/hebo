4bb611ed1b1590a0c97363b7206bfad51790ccdf
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-05-19 12:26:52 +0800

    fix typo

--- proxy/services/ai/conversations.py
+++ proxy/services/ai/conversations.py
@@ -397,7 +397,7 @@ async def execute_condense(
                         "Respond with the question only. "
                         "No comments or other text. "
                         "If only one question is present, respond with that question."
-                        "If the users is asking multiple questions in their last message, respond with all of them."
+                        "If the user is asking multiple questions in their last message, respond with all of them."
                     )
                 ),
             ],
