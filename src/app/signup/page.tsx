8a6e19b555d7117f0b333112b09b302d6dc485f3
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-06-03 18:45:10 +0800

    remove OAuthButtonGroup from signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,4 @@
-import { CredentialSignUp, OAuthButtonGroup } from "@stackframe/stack";
+import { CredentialSignUp } from "@stackframe/stack";
 import AuthLayout from "@/components/auth/AuthLayout";
 import Link from "next/link";
 
@@ -7,12 +7,6 @@ export default function CustomSignUpPage() {
     <AuthLayout>
       <div className="space-y-4">
         <CredentialSignUp />
-        <div className="flex items-center gap-4">
-          <div className="flex-1 h-[1px] bg-gray-200" />
-          <span className="text-sm whitespace-nowrap">or continue with</span>
-          <div className="flex-1 h-[1px] bg-gray-200" />
-        </div>
-        <OAuthButtonGroup type="sign-up" />
         <div className="w-full h-[1px] bg-gray-200 mt-4" />
         <p className="text-base text-left">
           Already have an account?{" "}
