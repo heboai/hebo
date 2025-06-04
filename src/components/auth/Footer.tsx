9d3d46961aa4d82244cd3a3b5eea86a9f5c953d9
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-06-04 10:26:56 +0800

    Remove api key from footer

--- src/components/auth/Footer.tsx
+++ src/components/auth/Footer.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import UserButtonWrapper from "./UserButtonWrapper";
-import { ApiKeyDisplay } from "./ApiKeyDisplay";
 
 export function Footer() {
     return (
         <div className="flex flex-col items-left gap-2">
             <UserButtonWrapper />
-            <ApiKeyDisplay />
         </div>
     );
 } 
