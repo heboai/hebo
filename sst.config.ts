419d612336529179d967c3f749c82096cc95d783
DavideSelvaggio <selvaggiodavide@hotmail.it>
2025-06-09 11:40:31 +0800

    migrate dns to aws

--- sst.config.ts
+++ sst.config.ts
@@ -7,17 +7,11 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
-      providers: input?.stage === "production" || input?.stage?.includes("preview") ? { cloudflare: "6.2.0" } : { aws: true },
     };
   },
   async run() {
-    const domain = $app.stage === "production"
-      ? { name: "cloud.hebo.ai", dns: sst.cloudflare.dns() }
-      : $app.stage.includes("preview")
-        ? { name: "cloud-" + $app.stage + ".hebo.ai", dns: sst.cloudflare.dns() }
-        : undefined;
     new sst.aws.Nextjs("HeboCloudApp", {
-      domain: domain
+      domain: "cloud.hebo.ai"
     });
   },
 });
