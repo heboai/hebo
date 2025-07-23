e3eefeaa979555d7a4fe78decb7d1addda2352ec
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-07-23 21:41:55 +0800

    Restyle signin page

--- apps/app/src/app/(auth)/signin/page.tsx
+++ apps/app/src/app/(auth)/signin/page.tsx
@@ -1,109 +1,87 @@
-import Image from "next/image";
-import Link from "next/link";
-
-import { Ban, BookCheck, CreditCard } from "lucide-react";
+import { Ban, BookOpen, CreditCard } from "lucide-react";
 import { MagicLinkSignIn, OAuthButtonGroup } from "@stackframe/react";
+
+import { Badge } from "@hebo/ui/components/Badge";
 import { Button } from "@hebo/ui/components/Button";
 
 import { Logo } from "~/components/ui/Logo";
 import { stackApp, StackProvider, StackTheme } from "~/lib/auth";
 
-
 export default function SignIn() {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      {/* Left side - Auth Components */}
-      <div className="w-full md:w-[38.125%] flex flex-col min-h-screen md:min-h-0 p-6 md:p-8">
-        <div className="flex flex-col flex-1">
-          {/* Main Content */}
-          <main className="flex-1 flex flex-col items-center justify-center">
-            <div className="w-full max-w-[400px]">
-              {/* Logo and Title */}
-              <Logo className="mb-2" />
+    <div className="min-h-screen flex flex-row">
 
-              {/* Description */}
-              <p className="text-base font-light mb-8">
-                The fastest way to build & scale agents
-              </p>
+      {/* Marketing Message */}
+      <aside className="hidden lg:block max-w-lg w-full  bg-blue-100 bg-[url(/login-bg.png)] bg-bottom-left bg-no-repeat">
 
-              {/* Auth Components */}
-              <StackProvider app={stackApp}>
-                <div className="space-y-4">
-                  <StackTheme>
-                    <OAuthButtonGroup type="sign-in" />
-                  </StackTheme>
-                  <div className="flex items-center gap-4">
-                    <div className="flex-1 h-[1px] bg-gray-200" />
-                    <span className="text-sm whitespace-nowrap">or continue with</span>
-                    <div className="flex-1 h-[1px] bg-gray-200" />
-                  </div>
-                  <StackTheme>
-                    <MagicLinkSignIn />
-                  </StackTheme>
-                  <div className="w-full h-[1px] bg-gray-200 mt-4" />
-                </div>
-              </StackProvider>
-
-              <div className="flex items-center gap-2 mb-2 mt-4">
-                <span className="flex items-center space-x-2">
-                  <CreditCard className="w-6 h-6" />
-                  <Ban className="w-6 h-6" />
-                </span>
-                <span className="text-base text-left">
-                  No credit card required
-                </span>
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
-
-      {/* Right side - Marketing Message */}
-      <aside className="
-      md:flex md:w-[61.875%]
-      items-center justify-center
-      px-[clamp(1.5rem,12vw,15rem)]
-      pt-[clamp(2rem,10vh,12.5rem)]
-      pb-[clamp(2rem,8vh,10rem)]
-      bg-blue-100
-      relative
-      ">
-        <Link
+        <Button 
+          asChild
+          className="absolute left-6 top-5 bg-blue-200 hover:bg-blue-300 text-foreground no-underline" 
+        >        
+          <a
           href="https://docs.hebo.ai"
           target="_blank"
           rel="noopener noreferrer"
-          className="absolute top-6 right-6"
-        >
-          <Button 
-            text="Docs" 
-            icon={<BookCheck className="h-5 w-5 shrink-0" />}
-            className="bg-blue-200 hover:bg-blue-300 text-foreground"
-          />
-        </Link>
-        <div className="max-w-[422px] space-y-6 text-left">
-          {/* decorative opening quote */}
-          <div className="relative">
-            <Image
-              src="/quote.png"
-              alt="Decorative Quote"
-              width={50}
-              height={50}
-              className="absolute -top-6 -left-6 select-none pointer-events-none z-0"
-            />
-            <p className="text-gray-500 text-xl font-regular leading-tight relative z-10">
-              The <span className="font-bold text-black">computing power</span> driving advances in <span className="font-bold text-black">generative AI</span> is projected to <span className="font-bold text-black">increase</span> by <span className="font-bold text-black">a millionfold</span> over the next decade.
-            </p>
-          </div>
+          >
+            <BookOpen />
+            Docs
+          </a>
+        </Button>
 
-          {/* quote author */}
-          <figcaption className="flex items-center justify-left gap-4">
-            <Image src="/huang.png" alt="" width={72} height={72} className="rounded-full" />
-            <div className="text-left">
-              <p className="text-lg text-gray-700">Jensen Huang <br /> CEO Nvidia</p>
-            </div>
-          </figcaption>
+        <div className="px-19 py-30 space-y-5 text-xl">
+          <div className="flex gap-2 text-3xl font-semibold items-center">
+            Hebo is{ }
+            <Badge className="bg-lime-400 text-foreground text-3xl font-semibold">FREE</Badge>
+          </div>
+          <div className="space-y-2">          
+            <div className="font-semibold">Deploy agents to production:</div>
+            <ul className="space-y-2">
+              <li>✔️ Choose from a set of free open-source models (Deepseek, Qwen, ...)</li>
+              <li>✔️ Use commercial models (Claude,  Gemini, ChatGPT) within fair-usage</li> 
+              <li>✔️ Link your custom inference endpoints to leverage existing credits</li> 
+            </ul>
+          </div>
+          <span className="font-semibold">Save money</span> on commercial models with our built-in cache and optimised routing to the cheapest cloud provider.
         </div>
       </aside>
+
+      {/* Login Components */}
+      <main className="w-full min-h-screen flex flex-1 items-center justify-center">
+    
+        <div className="w-xs flex flex-col gap-4 items-center">
+
+          <Logo />
+          <p className="text-base text-center">
+            The fastest way to build & scale agents
+          </p>
+
+          <StackProvider app={stackApp}>
+            <StackTheme>
+              <div className="w-full space-y-4">
+                <OAuthButtonGroup type="sign-in" />
+                <div className="flex items-center gap-4">
+                  <div className="flex-1 h-[1px] bg-gray-300" />
+                  <span className="text-sm whitespace-nowrap">or</span>
+                  <div className="flex-1 h-[1px] bg-gray-300" />
+                </div>
+                <MagicLinkSignIn />
+              </div>
+            </StackTheme>
+          </StackProvider>
+
+          <div className="flex items-center gap-2">
+            <span className="relative">
+              <Ban className="w-4 h-4" />
+              <CreditCard className="w-2 h-2 absolute left-1 top-1" />
+            </span>
+            <span className="text-sm">
+              No credit card required
+            </span>
+          </div>
+        </div>
+
+      </main>
+
     </div>
   );
 } 
