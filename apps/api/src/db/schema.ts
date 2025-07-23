1b0882113e54b0c80633ea67b093d61a365857c6
Sandyandi dela Cruz <sandyandi.delacruz@gmail.com>
2025-07-23 20:31:19 +0800

    Revert changes in API.

--- apps/api/src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { varchar, integer, pgTable as table } from 'drizzle-orm/pg-core';
-
-export const agents = table(
-  'agents',
-  {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar()
-  }
-);
