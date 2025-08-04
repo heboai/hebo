174f97954c15c3f233eabad506fbb29e111f54a3
Waazify <mhabaebi@gmail.com>
2025-08-04 12:39:11 +0800

    Rebase to main

---
+++ packages/ui/src/components/Select.tsx
@@ -0,0 +1,25 @@
+import {
+  Select as ShadCNSelect,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@hebo/ui/_shadcn/ui/select";
+
+export {
+  ShadCNSelect as Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+}; 
