c9b070e132d5571788fe8375013315bb3c75d736
turisanapo <selvaggiodavide@hotmail.it>
2025-08-06 17:47:45 +0200

    simplify model json schema

--- packages/db/schema/types/models.ts
+++ packages/db/schema/types/models.ts
@@ -10,34 +10,26 @@ export const SupportedProviders = Type.Union([
   Type.Literal("custom"),
 ]);
 
-// Custom endpoint schema
-export const CustomEndpointSchema = Type.Object({
+// Supported LLM models literal union
+export const SupportedLLMs = Type.Union([
+  // Todo: add more LLMs over time
+  Type.Literal("claude-3-5-sonnet-20241022"),
+  Type.Literal("claude-3-haiku-20240307"),
+  Type.Literal("claude-3-5-haiku-20241022"),
+]);
+
+// Endpoint schema
+export const EndpointSchema = Type.Object({
   url: Type.String({ format: "uri" }),
   provider: SupportedProviders,
   apiKey: Type.String({ minLength: 1 }),
 });
 
-// Routing schemas
-export const RoutingDefaultSchema = Type.Object({
-  // Todo: enable cheapest and fastest routing options in the future
-  type: Type.Literal("default"),
-});
-
-export const RoutingCustomSchema = Type.Object({
-  type: Type.Literal("custom"),
-  customEndpoint: CustomEndpointSchema,
-});
-
-export const RoutingSchema = Type.Union([
-  RoutingDefaultSchema,
-  RoutingCustomSchema,
-]);
-
 // Model schema
 export const ModelSchema = Type.Object({
   alias: Type.String({ minLength: 1 }),
-  LLM: Type.String({ minLength: 1 }),
-  routing: RoutingSchema,
+  LLM: SupportedLLMs, 
+  endpoint: Type.Optional(EndpointSchema),
 });
 
 // Collection of models must contain at least one entry
@@ -47,7 +39,7 @@ export const ModelsSchema = Type.Array(ModelSchema, { minItems: 1 });
 // Inferred Types
 // -----------------------------------------------------------------------------
 export type SupportedProvider = Static<typeof SupportedProviders>;
-export type CustomEndpoint = Static<typeof CustomEndpointSchema>;
-export type Routing = Static<typeof RoutingSchema>;
+export type SupportedLLM = Static<typeof SupportedLLMs>;
+export type Endpoint = Static<typeof EndpointSchema>;
 export type Model = Static<typeof ModelSchema>;
 export type Models = Static<typeof ModelsSchema>;
