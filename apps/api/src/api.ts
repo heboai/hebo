39d30e3873a0718769d8c79d492782db8c23f172
turisanapo <selvaggiodavide@hotmail.it>
2025-08-07 11:07:38 +0200

    prettier

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -2,6 +2,6 @@ import { db } from "@hebo/db/drizzle";
 import { branches } from "@hebo/db/schema/branches";
 
 export const handleGetVersion = async () => {
-    const result = await db.select().from(branches).execute();
-    return result;
+  const result = await db.select().from(branches).execute();
+  return result;
 };
