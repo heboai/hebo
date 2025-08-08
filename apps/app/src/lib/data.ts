7bb8db32f7adbce8015974baf53a48398936d795
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-08-08 18:33:38 +0800

    Eden: use edenTreaty in mock mode

--- apps/app/src/lib/data.ts
+++ apps/app/src/lib/data.ts
@@ -1,5 +1,3 @@
 import { edenTreaty } from "@elysiajs/eden";
 
-import type { Api } from "@hebo/api";
-
-export const api = edenTreaty<Api>(process.env.NEXT_PUBLIC_API_URL!);
+export const api = edenTreaty(process.env.NEXT_PUBLIC_API_URL!);
