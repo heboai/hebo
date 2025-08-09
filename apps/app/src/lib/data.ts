f6506484f2988a1bec890a7277785acef48eb217
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-08-09 18:29:53 +0800

    Hook up real backend

--- apps/app/src/lib/data.ts
+++ apps/app/src/lib/data.ts
@@ -1,10 +1,10 @@
-import { edenTreaty } from "@elysiajs/eden";
+import { treaty } from "@elysiajs/eden";
 
 import { Api } from "@hebo/api";
 
 import { isDevLocal } from "~/lib/env";
 
-
 const url = isDevLocal ? "/api" : process.env.NEXT_PUBLIC_API_URL!;
 
-export const api = edenTreaty<Api>(url);
+// TODO: test whether token is correctly passed on
+export const api = treaty<Api>(url, { fetch: { credentials: "include" } });
