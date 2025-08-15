4228f5974d4cb3957d059c79a4f9dc88daa7ee07
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-08-15 09:48:46 +0800

    Minimal schema factory with localized t.Omit use

--- apps/api/src/utils/schema-factory.ts
+++ apps/api/src/utils/schema-factory.ts
@@ -1,10 +1,4 @@
-import { Type } from "@sinclair/typebox";
-import {
-  createSchemaFactory as baseCreateSchemaFactory,
-  CreateSchemaFactoryOptions,
-} from "drizzle-typebox";
-
-import type { Table } from "drizzle-orm";
+export { createSchemaFactory } from "drizzle-typebox";
 
 export const AUDIT_FIELDS = [
   "createdBy",
@@ -16,29 +10,3 @@ export const AUDIT_FIELDS = [
 ] as const;
 
 export const ID_FIELDS = ["id", "slug"] as const;
-
-// TODO: We are breaking the type inference here. Fix this for the sake of consumers.
-export function createSchemaFactory(ops?: CreateSchemaFactoryOptions) {
-  const base = baseCreateSchemaFactory(ops);
-
-  const createSelectSchema = (entity: Table, keys?: readonly string[]) => {
-    const schema = base.createSelectSchema(entity);
-    return keys ? (ops?.typeboxInstance ?? Type).Omit(schema, keys) : schema;
-  };
-
-  const createInsertSchema = (entity: Table, keys?: readonly string[]) => {
-    const schema = base.createInsertSchema(entity);
-    return keys ? (ops?.typeboxInstance ?? Type).Omit(schema, keys) : schema;
-  };
-
-  const createUpdateSchema = (entity: Table, keys?: readonly string[]) => {
-    const schema = base.createUpdateSchema(entity);
-    return keys ? (ops?.typeboxInstance ?? Type).Omit(schema, keys) : schema;
-  };
-
-  return {
-    createSelectSchema,
-    createInsertSchema,
-    createUpdateSchema,
-  };
-}
