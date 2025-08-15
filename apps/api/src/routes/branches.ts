b31da60f339bec31ec6d43770b70209f94a21f0c
turisanapo <selvaggiodavide@hotmail.it>
2025-08-15 11:41:10 +0200

    implement route wip #2

--- apps/api/src/routes/branches.ts
+++ apps/api/src/routes/branches.ts
@@ -1,21 +1,27 @@
+import { eq } from "drizzle-orm";
 import { Elysia, t } from "elysia";
 
+import { db } from "@hebo/db";
+import { agents } from "@hebo/db/schema/agents";
 import { branches } from "@hebo/db/schema/branches";
 
 import { agentPathParam } from "~/routes/agents";
+import { createSlug } from "~/utils/create-slug";
 import {
   createSchemaFactory,
   AUDIT_FIELDS,
   ID_FIELDS,
 } from "~/utils/schema-factory";
 
-const { createInsertSchema, createUpdateSchema } = createSchemaFactory({
-  typeboxInstance: t,
-});
+const { createInsertSchema, createUpdateSchema, createSelectSchema } =
+  createSchemaFactory({
+    typeboxInstance: t,
+  });
 
 const _insertSchema = createInsertSchema(branches);
 const createBranch = createInsertSchema(branches);
 const updateBranch = createUpdateSchema(branches);
+const selectBranch = createSelectSchema(branches);
 
 const branchPathParams = t.Object({
   ...agentPathParam.properties,
@@ -28,47 +34,78 @@ export const branchRoutes = new Elysia({
 })
   .post(
     "/",
-    async ({ set }) => {
-      set.status = 501;
-      return "Not implemented" as const;
+    async ({ params, body, set }) => {
+      const agentResult = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.slug, params.agentSlug));
+      const agentId = agentResult[0].id;
+      const createdBy = "dummy";
+      const updatedBy = "dummy";
+      const slug = createSlug(body.name, false);
+      const [branch] = await db
+        .insert(branches)
+        .values({ agentId, ...body, slug, createdBy, updatedBy })
+        .returning();
+      set.status = 201;
+      return branch;
     },
     {
       params: agentPathParam,
       body: t.Omit(createBranch, [...AUDIT_FIELDS, ...ID_FIELDS, "agentId", "slug"]),
-      response: { 501: t.String() },
+      response: selectBranch,
     },
   )
   .get(
     "/",
-    async ({ set }) => {
-      set.status = 501;
-      return "Not implemented" as const;
+    async ({ params, set }) => {
+      const agentResult = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.slug, params.agentSlug));
+      const agentId = agentResult[0].id;
+      const branchList = await db
+        .select()
+        .from(branches)
+        .where(eq(branches.agentId, agentId));
+      set.status = 200;
+      return branchList;
     },
     {
       params: agentPathParam,
-      response: { 501: t.String() },
+      response: [selectBranch],
     },
   )
   .get(
     "/:branchSlug",
-    async ({ set }) => {
-      set.status = 501;
-      return "Not implemented" as const;
+    async ({ params, set }) => {
+      const [branch] = await db
+        .select()
+        .from(branches)
+        .where(eq(branches.slug, params.branchSlug));
+      set.status = 200;
+      return branch;
     },
     {
       params: branchPathParams,
-      response: { 501: t.String() },
+      response: selectBranch,
     },
   )
   .put(
     "/:branchSlug",
-    async ({ set }) => {
-      set.status = 501;
-      return "Not implemented" as const;
+    async ({ body, params, set }) => {
+      const updatedBy = "dummy";
+      const [branch] = await db
+        .update(branches)
+        .set({ ...body, updatedBy })
+        .where(eq(branches.slug, params.branchSlug))
+        .returning();
+      set.status = 200;
+      return branch;
     },
     {
       params: branchPathParams,
       body: t.Omit(updateBranch, [...AUDIT_FIELDS, ...ID_FIELDS, "agentId", "slug"]),
-      response: { 501: t.String() },
+      response: selectBranch,
     },
   );
