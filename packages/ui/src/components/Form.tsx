1e4567f460669e075a3ce774b4fa7da8227b9c40
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-08-25 22:04:20 +0200

    Migrate from React Hook Forms to Conform for better React Router ecosystem alignment

--- packages/ui/src/components/Form.tsx
+++ packages/ui/src/components/Form.tsx
@@ -1,10 +1,92 @@
-export {
-  useFormField,
-  Form,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormDescription,
-  FormMessage,
-  FormField,
-} from "../_shadcn/ui/form";
+import { type FieldMetadata } from "@conform-to/react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { Slot } from "@radix-ui/react-slot";
+import * as React from "react";
+
+import { Label } from "../_shadcn/ui/label";
+import { cn } from "../lib/utils";
+
+// TODO: does this actually work
+const FieldCtx = React.createContext<FieldMetadata<any> | undefined>(undefined);
+const useField = () => {
+  const f = React.useContext(FieldCtx);
+  if (!f)
+    throw new Error(
+      "Use <FormLabel/FormControl/FormDescription/FormMessage> inside <FormField>.",
+    );
+  return f;
+};
+
+function FormField({
+  field,
+  children,
+}: {
+  field: FieldMetadata<any>;
+  children: React.ReactNode;
+}) {
+  return <FieldCtx.Provider value={field}>{children}</FieldCtx.Provider>;
+}
+
+function FormLabel({
+  className,
+  ...props
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  const { id, valid } = useField();
+
+  return (
+    <Label
+      data-slot="form-label"
+      data-error={!valid}
+      className={cn("data-[error=true]:text-destructive", className)}
+      htmlFor={id}
+      {...props}
+    />
+  );
+}
+
+function FormDescription({ className, ...props }: React.ComponentProps<"p">) {
+  const { descriptionId } = useField();
+
+  return (
+    <p
+      data-slot="form-description"
+      id={descriptionId}
+      className={cn("text-muted-foreground text-sm", className)}
+      {...props}
+    />
+  );
+}
+
+function FormMessage({ className, ...props }: React.ComponentProps<"p">) {
+  const { errorId, errors } = useField();
+
+  return (
+    <p
+      data-slot="form-message"
+      id={errorId}
+      className={cn("text-destructive text-sm", className)}
+      {...props}
+    >
+      {errors}
+    </p>
+  );
+}
+
+function FormControl({ ...props }: React.ComponentProps<typeof Slot>) {
+  const { id, defaultValue, valid, descriptionId, errorId } = useField();
+
+  return (
+    <Slot
+      data-slot="form-control"
+      id={id}
+      // TODO: does the defaultValue work?
+      // TODO: what about name?
+      defaultValue={defaultValue}
+      aria-describedby={valid ? `${descriptionId}` : `${errorId}`}
+      aria-invalid={!valid}
+      {...props}
+    />
+  );
+}
+
+export { FormControl, FormField, FormLabel, FormDescription, FormMessage };
--- packages/ui/src/components/Select.tsx
+++ packages/ui/src/components/Select.tsx
@@ -1,12 +1,65 @@
-export {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectScrollDownButton,
-  SelectScrollUpButton,
-  SelectSeparator,
+import { useControl } from "@conform-to/react/future";
+import { useRef } from "react";
+
+import {
   SelectTrigger,
+  Select as ShadcnSelect,
   SelectValue,
+  SelectContent,
+  SelectItem,
 } from "../_shadcn/ui/select";
+
+type SelectProps = {
+  id?: string;
+  name: string;
+  items: Array<{ name: string; value: string }>;
+  placeholder: string;
+  defaultValue?: string;
+  ["aria-describedby"]?: string;
+};
+
+function Select({
+  name,
+  items,
+  placeholder,
+  defaultValue,
+  ...props
+}: SelectProps) {
+  const selectRef = useRef<React.ElementRef<typeof SelectTrigger>>(null);
+  const control = useControl({
+    defaultValue,
+    onFocus() {
+      selectRef.current?.focus();
+    },
+  });
+
+  return (
+    <>
+      <input name={name} ref={control.register} hidden />
+      <ShadcnSelect
+        value={control.value}
+        onValueChange={(value) => control.change(value)}
+        onOpenChange={(open) => {
+          if (!open) {
+            control.blur();
+          }
+        }}
+      >
+        <SelectTrigger {...props} ref={selectRef}>
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          {items.map((item) => {
+            return (
+              <SelectItem key={item.value} value={item.value}>
+                {item.name}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </ShadcnSelect>
+    </>
+  );
+}
+
+export { Select };
