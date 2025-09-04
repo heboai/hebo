391977c03d5ddbe2691f33de31bd26adf6654b97
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-09-03 19:44:56 -0700

    getNodeText: Follow eslint style guides

--- packages/ui/src/components/code/utils/getNodeText.ts
+++ packages/ui/src/components/code/utils/getNodeText.ts
@@ -1,16 +1,19 @@
+import { ReactElement, ReactNode, isValidElement } from "react";
+
 // Gets the text from a component as if you selected it with a mouse and copied it.
-export const getNodeText = (node): string => {
+export const getNodeText = (node: ReactNode): string => {
   if (["string", "number"].includes(typeof node)) {
-    // Convert number into string
-    return node.toString();
+    return String(node);
   }
 
   if (Array.isArray(node)) {
     return node.map((child) => getNodeText(child)).join("");
   }
 
-  if (typeof node === "object" && node?.props?.children) {
-    return getNodeText(node.props.children);
+  if (isValidElement(node)) {
+    return getNodeText(
+      (node as ReactElement<{ children?: ReactNode }>).props.children,
+    );
   }
 
   return "";
