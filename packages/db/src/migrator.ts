aa4a3af8a0d48948bc7003209679c826955602eb
turisanapo <selvaggiodavide@hotmail.it>
2025-09-09 13:02:30 +0800

    remove iflocal check from migration handler

--- packages/db/src/migrator.ts
+++ packages/db/src/migrator.ts
@@ -1,16 +1,11 @@
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 
 import { db } from "../drizzle";
-import { isLocal } from "../runtime-config";
 
 import type { NodePgDatabase } from "drizzle-orm/node-postgres";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler = async (event: unknown) => {
-  if (isLocal) {
-    throw new Error("Migrations must run against Postgres, not PGlite.");
-  }
-
   await migrate(db as unknown as NodePgDatabase, {
     migrationsFolder: "./migrations",
   });
