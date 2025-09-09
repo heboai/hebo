60205e9a7d0341a6d5972d4adf486e89d148a7f4
turisanapo <selvaggiodavide@hotmail.it>
2025-09-09 16:09:11 +0800

    fix port forwarding and log console colors

--- infra/stacks/api.ts
+++ infra/stacks/api.ts
@@ -21,23 +21,24 @@ const heboApiService = new sst.aws.Service("HeboApiService", {
   },
   environment: {
     LOG_LEVEL: $app.stage === "production" ? "info" : "debug",
+    NO_COLOR: "1",
+    FORCE_COLOR: "0",
+    CLICOLOR: "0",
     PG_DATABASE: heboDatabase.database,
     PG_HOST: heboDatabase.host,
     PG_PASSWORD: secrets.dbPassword.value,
     PG_PORT: heboDatabase.port.apply((port) => port.toString()),
     PG_USER: heboDatabase.username,
     PGSSLMODE: "require",
-    PORT: "80",
+    PORT: "8080",
     STACK_SECRET_SERVER_KEY: secrets.stackSecretServerKey.value,
     VITE_STACK_PROJECT_ID: secrets.stackProjectId.value,
   },
   loadBalancer: {
     domain: apiDomainName,
     rules: [
-      {
-        listen: "80/http",
-        redirect: "443/https",
-      },
+      { listen: "80/http", redirect: "443/https" },
+      { listen: "443/https", forward: "8080/http" },
     ],
   },
   scaling: {
