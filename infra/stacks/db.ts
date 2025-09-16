5d88744797993ad2d49a8457c1954ded2ae9b7d0
turisanapo <selvaggiodavide@hotmail.it>
2025-09-16 18:59:59 +0800

    use sst.Resource for lambda

--- infra/stacks/db.ts
+++ infra/stacks/db.ts
@@ -30,20 +30,7 @@ const heboDatabase = new sst.aws.Aurora("HeboDatabase", {
 const migrator = new sst.aws.Function("DatabaseMigrator", {
   handler: "packages/db/lambda/migrator.handler",
   vpc: heboVpc,
-  // Required for reading secrets from SSM
-  layers: [
-    "arn:aws:lambda:us-east-1:177933569100:layer:AWS-Parameters-and-Secrets-Lambda-Extension:19",
-  ],
-  permissions: [
-    {
-      actions: ["ssm:GetParameter", "ssm:GetParameters"],
-      resources: [vars.dbUsername.arn, vars.dbPassword.arn],
-    },
-    {
-      actions: ["kms:Decrypt"],
-      resources: ["*"],
-    },
-  ],
+  link: [heboDatabase],
   copyFiles: [
     {
       from: "packages/db/migrations",
@@ -53,10 +40,11 @@ const migrator = new sst.aws.Function("DatabaseMigrator", {
   environment: {
     NODE_EXTRA_CA_CERTS: "/var/runtime/ca-cert.pem",
     PG_HOST: heboDatabase.host,
-    PG_PASSWORD_SSM_NAME: vars.dbPassword.name,
-    PG_USER_SSM_NAME: vars.dbUsername.name,
     PG_PORT: heboDatabase.port.apply((port) => port.toString()),
     PG_DATABASE: heboDatabase.database,
+  },
+  nodejs: {
+    install: ["sst"],
   },
 });
 
--- packages/db/lambda/migrator.ts
+++ packages/db/lambda/migrator.ts
@@ -1,35 +1,14 @@
 import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { Resource } from "sst";
 
 import { createDb } from "../drizzle";
 
 import type { NodePgDatabase } from "drizzle-orm/node-postgres";
 
-const SSM_PORT = process.env.PARAMETERS_SECRETS_EXTENSION_HTTP_PORT || "2773";
-const SSM_BASE = `http://localhost:${SSM_PORT}`;
-
-async function getParam(name: string) {
-  const res = await fetch(
-    `${SSM_BASE}/systemsmanager/parameters/get?name=${encodeURIComponent(name)}&withDecryption=true`,
-    {
-      headers: {
-        "X-Aws-Parameters-Secrets-Token": process.env.AWS_SESSION_TOKEN!,
-      },
-    },
-  );
-  if (!res.ok) {
-    const body = await res.text();
-    throw new Error(`SSM getParam failed (${res.status}): ${body}`);
-  }
-  const json = (await res.json()) as { Parameter: { Value: string } };
-  return json.Parameter.Value;
-}
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handler = async (event: unknown) => {
-  const [dbUser, dbPassword] = await Promise.all([
-    getParam(process.env.PG_USER_SSM_NAME!),
-    getParam(process.env.PG_PASSWORD_SSM_NAME!),
-  ]);
+  const dbUser = Resource.HeboDatabase.username;
+  const dbPassword = Resource.HeboDatabase.password;
   const db = await createDb(dbUser, dbPassword);
   await migrate(db as unknown as NodePgDatabase, {
     migrationsFolder: "./migrations",
