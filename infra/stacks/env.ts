8b65d657db79b21434a22c8507a0b6698605ef56
turisanapo <selvaggiodavide@hotmail.it>
2025-09-20 10:53:43 +0800

    dbUsername and dbPassword as optional in dev

--- infra/stacks/env.ts
+++ infra/stacks/env.ts
@@ -6,8 +6,8 @@ export const stackPublishableClientKey = new sst.Secret(
 );
 export const stackProjectId = new sst.Secret("StackProjectId", "");
 // Database
-export const dbUsername = new sst.Secret("DbUsername");
-export const dbPassword = new sst.Secret("DbPassword");
+export const dbUsername = new sst.Secret("DbUsername", "");
+export const dbPassword = new sst.Secret("DbPassword", "");
 // LLMs
 export const groqApiKey = new sst.Secret("GroqApiKey");
 export const voyageApiKey = new sst.Secret("VoyageApiKey");
