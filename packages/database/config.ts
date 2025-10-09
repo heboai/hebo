7cdb48f65170f747401895c080045fad0295bc1c
turisanapo <selvaggiodavide@hotmail.it>
2025-10-09 09:03:58 +0800

    do not use any to silence typescript error

--- packages/database/config.ts
+++ packages/database/config.ts
@@ -2,8 +2,8 @@ import { Resource } from "sst";
 
 export const connectionString = (() => {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const db = (Resource as any).HeboDatabase;
+    // @ts-expect-error: HeboDatabase may not be defined
+    const db = Resource.HeboDatabase;
     return `postgresql://${db.username}:${db.password}@${db.host}:${db.port}/${db.database}?sslmode=verify-full`;
   } catch {
     return "postgresql://postgres:password@localhost:5432/local";
