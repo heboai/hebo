e4a685775558eca71d2fce2701e2df0669b80889
turisanapo <selvaggiodavide@hotmail.it>
2025-10-14 15:29:28 +0800

    add comment

--- packages/shared-api/middlewares/db-client.ts
+++ packages/shared-api/middlewares/db-client.ts
@@ -2,6 +2,7 @@ import { Elysia } from "elysia";
 
 import { createDbClient } from "@hebo/database/client";
 
+// Note: Must be used after authService to ensure userId is set
 export const dbClient = new Elysia({
   name: "db-client",
 })
