7ebb79c0168270c08c79b36d02100a15c8229bcc
turisanapo <selvaggiodavide@hotmail.it>
2025-10-25 11:50:07 +0800

    use exact match as defined in config

--- apps/console/app/lib/env.ts
+++ apps/console/app/lib/env.ts
@@ -1,6 +1,6 @@
 export const isStackAuthEnabled = !!import.meta.env.VITE_STACK_PROJECT_ID;
 export const isDevLocal =
-  !process.env?.TURBO_HASH &&
+  !process.env.TURBO_HASH &&
   !import.meta.env.VITE_API_URL &&
   !import.meta.env.VITE_GATEWAY_URL;
 export const isDev = import.meta.env.MODE === "development" && !isDevLocal;
