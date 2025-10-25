d709f3e70d633fa9cffc9d91dac6de2f84efd809
turisanapo <selvaggiodavide@hotmail.it>
2025-10-25 11:44:48 +0800

    localdev only when service urls are not set

--- apps/console/app/lib/env.ts
+++ apps/console/app/lib/env.ts
@@ -1,3 +1,6 @@
 export const isStackAuthEnabled = !!import.meta.env.VITE_STACK_PROJECT_ID;
-export const isDevLocal = !process.env?.TURBO_HASH;
+export const isDevLocal =
+  !process.env?.TURBO_HASH &&
+  !import.meta.env.VITE_API_URL &&
+  !import.meta.env.VITE_GATEWAY_URL;
 export const isDev = import.meta.env.MODE === "development" && !isDevLocal;
