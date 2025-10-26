bea9205e2796565b7bc4e6dc1b99455b063eed1e
turisanapo <selvaggiodavide@hotmail.it>
2025-10-26 17:23:24 +0800

    fix sst secrets

--- infra/stacks/env.ts
+++ infra/stacks/env.ts
@@ -23,6 +23,7 @@ export const allSecrets = [
   stackProjectId,
   accessKeyId,
   secretAccessKey,
+  bedrockInferenceProfile,
   googleVertexServiceAccount,
   googleVertexProject,
   voyageApiKey,
