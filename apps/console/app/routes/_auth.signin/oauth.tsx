47a0b68831b1ba28a69d3cda5d53507c0754f1bf
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-11-21 12:02:25 +0800

    feat(console): implement custom oauth components

--- apps/console/app/routes/_auth.signin/oauth.tsx
+++ apps/console/app/routes/_auth.signin/oauth.tsx
@@ -1,27 +1,98 @@
-import { lazy } from "react";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 import { Button } from "@hebo/shared-ui/components/Button";
 
 import { isStackAuthEnabled } from "~console/lib/env";
+import { getStackApp } from "~console/lib/auth/stack-auth";
+import React from "react";
 
-const StackOAuthSignIn = lazy(() =>
-  import("@stackframe/react").then((mod) => ({
-    default: mod.OAuthButtonGroup,
-  })),
-);
 
 export function OAuthSignIn() {
-  return isStackAuthEnabled ? (
-    <StackOAuthSignIn type="sign-in" />
-  ) : (
-    <Button asChild variant="outline" className="w-full">
-      <Link
-        to="/"
-        viewTransition
-      >
-        Dummy OAuth SignIn
-      </Link>
+  if (isStackAuthEnabled) {
+    const stackApp = getStackApp();
+    const redirect = async (provider: string) => stackApp.signInWithOAuth(provider);
+
+    return (
+      <>
+        <SignInButton provider="github" icon={GithubIcon} redirect={redirect}/>
+        <SignInButton provider="google" icon={GoogleIcon} redirect={redirect}/>
+        <SignInButton provider="microsoft" icon={MicrosoftIcon} redirect={redirect}/>
+      </>
+    )
+  }
+  
+  const navigate = useNavigate();
+  const redirect = async () => navigate("/");
+
+  return (
+    <SignInButton provider="dummy" redirect={redirect}/>
+  );
+}
+
+function SignInButton({ provider, icon: Icon, redirect }: { provider: string, icon?: React.ComponentType, redirect: (provider: string) => Promise<void> }) {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  return (
+    <Button
+      variant="outline"
+      className="w-full"
+      isLoading={isLoading}
+      onClick={async () => {
+        try {
+          setIsLoading(true);
+          await redirect(provider);
+        } finally {
+          setIsLoading(false);
+        }
+      }}
+    >
+      {!isLoading && (
+        <>
+          { Icon && <Icon /> }
+          Sign in with {provider.charAt(0).toUpperCase()}{provider.slice(1)}
+        </>
+      )
+      }
     </Button>
+  );
+}
+
+
+function GithubIcon({ width = 24, height = 24}) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {/* … SVG path data copied from the Microsoft logo file … */}
+    </svg>
+  );
+}
+
+function GoogleIcon({ width = 24, height = 24}) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {/* … SVG path data copied from the Microsoft logo file … */}
+    </svg>
+  );
+}
+
+function MicrosoftIcon({ width = 24, height = 24}) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {/* … SVG path data copied from the Microsoft logo file … */}
+    </svg>
   );
 }
