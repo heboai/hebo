021c96454c0062ce4e6aeb997e9418ae07af5230
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-11-21 15:43:44 +0800

    fix(console): CodeRabbit fixes

--- apps/console/app/routes/_auth.signin/oauth.tsx
+++ apps/console/app/routes/_auth.signin/oauth.tsx
@@ -16,29 +16,35 @@ export function OAuthSignIn() {
 
 function SignInButton({ provider, icon: Icon }: { provider: string, icon?: React.ComponentType }) {
   const [isLoading, setIsLoading] = React.useState(false);
+  const [ error, setError ] = React.useState<string | undefined>();
 
   return (
-    <Button
-      variant="outline"
-      className="relative w-full"
-      isLoading={isLoading}
-      onClick={async () => {
-        try {
-          setIsLoading(true);
-          await authService.signInWithOAuth(provider);
-        } finally {
-          setIsLoading(false);
+    <>
+      <Button
+        variant="outline"
+        className="relative w-full"
+        isLoading={isLoading}
+        onClick={async () => {
+          try {
+            setIsLoading(true);
+            await authService.signInWithOAuth(provider);
+          } catch (error) {
+            error instanceof Error && setError(error.message);
+          } finally {
+            setIsLoading(false);
+          }
+        }}
+      >
+        {!isLoading && (
+          <>
+            { Icon && <span className="absolute left-4"><Icon aria-hidden="true" /></span> }
+            Sign in with {provider.charAt(0).toUpperCase()}{provider.slice(1)}
+          </>
+        )
         }
-      }}
-    >
-      {!isLoading && (
-        <>
-          { Icon && <span className="absolute left-4"><Icon aria-hidden="true" /></span> }
-          Sign in with {provider.charAt(0).toUpperCase()}{provider.slice(1)}
-        </>
-      )
-      }
-    </Button>
+      </Button>
+      {error && <div className="text-destructive text-sm">{error}</div>}
+    </>
   );
 }
 
